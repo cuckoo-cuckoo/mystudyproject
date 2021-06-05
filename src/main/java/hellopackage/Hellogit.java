4432e8e51123ee176435ece407d73ede13c08597
cuckoo-cuckoo <1479507673@qq.com>
2021-06-05 11:45:14 +0800

    逻辑的优化

--- src/main/java/hellopackage/Hellogit.java
+++ src/main/java/hellopackage/Hellogit.java
@@ -4,5 +4,9 @@ public class Hellogit {
      public static void main(String[] args) {
 		  System.out.println("Hellogit");
                  System.out.println("你好啊");
+                 System.out.println("你好啊");
+                 System.out.println("你好啊");
+                 System.out.println("你好啊");
+                 System.out.println("你好啊");
 	}
 }
