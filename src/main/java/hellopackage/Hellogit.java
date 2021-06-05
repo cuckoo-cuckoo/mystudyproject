832170eccd8b53e0f75a7a71015d9584431b0d3d
cuckoo-cuckoo <1479507673@qq.com>
2021-06-05 13:59:41 +0800

    版本dev-zhang002

--- src/main/java/hellopackage/Hellogit.java
+++ src/main/java/hellopackage/Hellogit.java
@@ -2,7 +2,7 @@ package hellopackage;
 
 public class Hellogit {
      public static void main(String[] args) {
-		  System.out.println("Hellogit");
+                 System.out.println("Hellogit");
                  System.out.println("你好啊");
                  System.out.println("你好啊");
                  System.out.println("你好啊");
